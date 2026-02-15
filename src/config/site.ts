25e8bdb71719776c77867554e16073ff4ebb1448
Markus Lachinger <business@mmlac.com>
2026-02-14 17:50:07 -0800

    Setup Tessera website

---
+++ src/config/site.ts
@@ -0,0 +1,4 @@
+export const site = {
+  title: "Tessera — Powerful Twitch Desktop App",
+  description: "Tessera is a powerful Twitch desktop app that lets you watch videos and chat in the same place.",
+} as const;
